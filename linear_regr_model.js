27c992857d7019dc5dcac6f5d05a91026806afa2
TienNG4360 <114276269+TienNG4360@users.noreply.github.com>
2023-12-21 17:58:22 +0800

    Add files via upload

---
+++ linear_regr_model.js
@@ -0,0 +1,9 @@
+function predictLinearRegr(inputValue) {
+    // Thay thế đoạn mã dưới đây bằng nội dung của file linear_regr_model.py
+    // var linearRegrModelCode = "YOUR_LINEAR_REGR_MODEL_CODE";
+    // eval(linearRegrModelCode);
+    
+    // Gọi hàm dự đoán từ mô hình đã chuyển đổi
+    var prediction = predict(inputValue);
+    return prediction;
+}
